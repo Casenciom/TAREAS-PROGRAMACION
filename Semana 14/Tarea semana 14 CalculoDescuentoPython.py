0601ded0d9ac3384585c7af445f18f3eb8c67f60
Casenciom <ca.asenciom@uea.edu.ec>
2025-03-21 23:08:03 -0500

    Tarea semana 14 CalculoDescuentoPython

--- Semana 14/Tarea semana 14 CalculoDescuentoPython.py
+++ Semana 14/Tarea semana 14 CalculoDescuentoPython.py
@@ -6,10 +6,10 @@ if __name__=="__main__":
     compras_mensuales = 4500
     compra_moto = 3000
 
-## Llamada Monto 1
+## Llamada Compras mensuales
     descuento1 = calcular_descuento(compras_mensuales)
     print(f"Monto de la compra mensuales es {compras_mensuales}, el descuento es {descuento1}")
 
-## Llamada Monto 2
+## Llamada Compra de una moto
     descuento2 = calcular_descuento(compra_moto)
     print(f"Monto de la compra de la moto es {compra_moto}, el descuento es {descuento2}")
